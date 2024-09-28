3e34fdc5d69b71398d37f7241b62402babd159e4
Matheus <matheusfernandesluz@gmail.com>
2024-09-28 12:21:02 -0400

    fist commit

---
+++ modules/vpc/main.tf
@@ -0,0 +1,46 @@
+resource "aws_vpc" "new-vpc" {
+  cidr_block = var.vpc_cidr_block
+  # enable_dns_support = true
+  # enable_dns_hostnames = true
+  
+  tags = {
+    Name = "${var.prefix}-vpc"
+  }
+}
+
+data "aws_availability_zones" "available" {}
+
+resource "aws_subnet" "subnets" {
+  count = 2
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+  vpc_id = aws_vpc.new-vpc.id
+  cidr_block = "10.0.${count.index + 1}.0/24"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "${var.prefix}-subnet-${count.index + 1}"
+  }
+}
+
+resource "aws_internet_gateway" "new-internet-gateway" {
+  vpc_id = aws_vpc.new-vpc.id
+  tags = {
+    Name = "${var.prefix}-igw"
+  }
+}
+
+resource "aws_route_table" "new-route-table" {
+  vpc_id = aws_vpc.new-vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.new-internet-gateway.id
+  }
+  tags = {
+    Name = "${var.prefix}-rtb"
+  }
+}
+
+resource "aws_route_table_association" "new-rtb-association" {
+  count = 2
+  route_table_id = aws_route_table.new-route-table.id
+  subnet_id = aws_subnet.subnets[count.index].id
+}

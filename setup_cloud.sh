7e3a275e04d3d5a4138466e8b45576d6e6cac7d3
Matheus <matheusfernandesluz@gmail.com>
2024-11-30 12:55:27 -0400

    atuliza script para geração de buckets e ECR.

--- setup_cloud.sh
+++ setup_cloud.sh
@@ -4,12 +4,16 @@
 REGION="us-east-1"
 
 # Defina os nomes dos buckets (devem ser únicos globalmente)
-BUCKET_NAME_1="tc-infra-cloud-bucket"
-BUCKET_NAME_2="tc-infra-database-bucket"
-BUCKET_NAME_3="tc-api-bucket"
+BUCKET_NAME_1="pediaqui-infra-cloud-bucket"
+BUCKET_NAME_2="pediaqui-infra-database-bucket"
+BUCKET_NAME_3="pediaqui-catalog-bucket"
+BUCKET_NAME_4="pediaqui-payment-bucket"
+BUCKET_NAME_5="pediaqui-order-bucket"
 
-# Defina o nome do repositório ECR
-ECR_REPOSITORY_NAME="tech-challenge-image"
+# Defina os nomes dos repositórios ECR
+ECR_REPOSITORY_NAME_1="pediaqui-catalog-image"
+ECR_REPOSITORY_NAME_2="pediaqui-order-image"
+ECR_REPOSITORY_NAME_3="pediaqui-payment-image"
 
 # Função para criar um bucket S3
 create_bucket() {
@@ -28,24 +32,29 @@ create_bucket() {
 
 # Função para criar um repositório ECR
 create_ecr_repository() {
-    echo "Criando repositório ECR: $ECR_REPOSITORY_NAME na região $REGION..."
+    REPOSITORY_NAME=$1
+    echo "Criando repositório ECR: $REPOSITORY_NAME na região $REGION..."
     
     # Comando AWS CLI para criar o repositório ECR
-    aws ecr create-repository --repository-name "$ECR_REPOSITORY_NAME" --region "$REGION" --no-cli-pager
+    aws ecr create-repository --repository-name "$REPOSITORY_NAME" --region "$REGION" --no-cli-pager
     
     if [ $? -eq 0 ]; then
-        echo "Repositório ECR $ECR_REPOSITORY_NAME criado com sucesso!"
+        echo "Repositório ECR $REPOSITORY_NAME criado com sucesso!"
     else
-        echo "Falha ao criar o repositório ECR $ECR_REPOSITORY_NAME."
+        echo "Falha ao criar o repositório ECR $REPOSITORY_NAME."
     fi
 }
 
-# Criar os 3 buckets
+# Criar os 5 buckets
 create_bucket "$BUCKET_NAME_1"
 create_bucket "$BUCKET_NAME_2"
 create_bucket "$BUCKET_NAME_3"
+create_bucket "$BUCKET_NAME_4"
+create_bucket "$BUCKET_NAME_5"
 
-# Criar o repositório ECR
-create_ecr_repository
+# Criar os 5 repositórios ECR
+create_ecr_repository "$ECR_REPOSITORY_NAME_1"
+create_ecr_repository "$ECR_REPOSITORY_NAME_2"
+create_ecr_repository "$ECR_REPOSITORY_NAME_3"
 
 echo "Script finalizado."
